d3ecd493b28a31e4de11bb4cd7c4731232c439f9
Seungho <seungho@Seunghos-MacBook-Pro.local>
2019-06-13 20:17:26 +0900

    2nd commit.

--- HIST_COMP.cc
+++ HIST_COMP.cc
@@ -25,68 +25,29 @@ using namespace std;
 // 	Canvas with two histograms piled up
 TCanvas *canvas_pile(string h_name, TH1D *h1, TH1D *h2)
 {
-	gStyle->SetOptStat(0);
 	TCanvas *c_pile = new TCanvas(h_name.c_str());
 
-	double h1_max = h1->GetMaximum();
-	double h2_max = h2->GetMaximum();
-	double h1_min = h1->GetMinimum();
-	double h2_min = h2->GetMinimum();
-
-	double h_min = h1_min < h2_min ? h1_min : h2_min;
-	double h_max = h1_max > h2_max ? h1_max : h2_max;
-
-	// 	Setting the appropriate Y range for the histograms
-	//h1->GetYaxis()->SetRangeUser(h_min * 0.8, h_max * 1.2);
-	//h2->GetYaxis()->SetRangeUser(h_min * 0.8, h_max * 1.2);
-	
 	h1->SetLineColor(kRed);
+	//h1->SetLineWidth(2);
+	h1->SetLineStyle(2);
+	
+	h1->SetStats(0);
+	h2->SetStats(0);
+
 	h2->SetLineColor(kBlue);
-	
-	//TH1D *h_ratio = h2->Clone();
-	//h_ratio->Divide(h1);
+	//h2->SetLineWidth(3);
 
-	//double mean = h_ratio->GetMean();
-	//double stdev = h_ratio->GetRMS();
+	TLegend *legend = new TLegend(0.6, 0.6, 0.85, 0.85);
 
-	//cout << "mean: " << mean << " stdev: " << stdev << endl;
-	h2->SetLineWidth(3);
-	h2->Draw("hist");
-	//h1->Draw("same hist");
+	h1->GetXaxis()->SetTitle(h_name.substr(1).c_str());
+	h1->Draw("hist");
+	h2->Draw("same hist");
 
-	TLegend *legend = new TLegend(0.7, 0.6, 0.9, 0.9);
-	//legend->AddEntry(h1, "MC", "l");
 	legend->AddEntry(h2, "Data", "l");
-
-	if(h2->GetMaximum() > 0.0){
-		TH1D *h3 = (TH1D*)h1->Clone();
-		TH1D *h_err = (TH1D*)h2->Clone();
-		
-		//cout << "h1: " << h1 << " h2: " << h2 << endl;
-		h3->Scale(coefficient(h1, h2));
-		cout << "hname: " << h_name << endl;
-		cout << "data max: " << h2->GetMaximum() << " mc max: " << h1->GetMaximum() << endl;
-		cout << "data max bin: " << h2->GetMaximumBin() << " mc max bin: " << h1->GetMaximumBin() << endl;
-		
-		h3->SetLineColor(kRed);
-		h3->SetLineWidth(2);
-		h3->SetLineStyle(2);
-		h_err->Add(h1, -1);
+	legend->AddEntry(h1, "Normalized MC", "l");
 	
-		h3->Draw("same hist");
-		//h2->Draw("same hist");
-		legend->AddEntry(h3, "Normalized MC", "l");
-		h1->SetStats(0);
-		h2->SetStats(0);
-		h3->SetStats(0);
-		c_pile->Update();
-
-	//	TLatex *l = new TLatex();
-	//	l->DrawLatex(2, 0.3, Form("Error size: %f", h_err->GetRMS()));
-	}
-
 	legend->Draw();
-	gStyle->SetOptStat(0);
+	c_pile->Update();
 
 	return c_pile;
 }
@@ -94,15 +55,48 @@ TCanvas *canvas_pile(string h_name, TH1D *h1, TH1D *h2)
 
 
 // 	Write MC and Data histograms in each directory
-void write_histograms(string *names, TDirectory *MC, TDirectory *DA)
+void write_histograms(vector<string> var, TDirectory *MC, TDirectory *DA, const char* option = "")
 {
-	for(int i = 0; i < 28; i++){
-		TH1D *h_mc = (TH1D*)MC->Get(names[i].c_str());
-		TH1D *h_da = (TH1D*)DA->Get(names[i].c_str());
+	TH1D *h_mc_mcn, *h_mc_bkg, *h_mc, *h_da;
+	int number_of_hists = var.size();
 	
-		TCanvas *c_pile = canvas_pile(names[i], h_mc, h_da);
-		c_pile->Write();
+	TCanvas *c_pile[number_of_hists];
+
+	for(int i = 0; i < number_of_hists; i++){
+		if(!strcmp(option, "other")){
+			h_mc = (TH1D*)MC->Get((var[i] + "_all").c_str());
+			h_da = (TH1D*)DA->Get((var[i] + "_all").c_str());
+		}
+		else{
+			h_mc_mcn = (TH1D*)MC->Get((var[i] + "_mcn").c_str());
+			h_mc_bkg = (TH1D*)MC->Get((var[i] + "_bkg").c_str());
+	
+			h_da = (TH1D*)DA->Get((var[i] + "_bkg").c_str());
+	
+			h_mc = (TH1D*)h_mc_mcn->Clone();
+			h_mc->Add(h_mc_bkg);
+		}
+	
+		// 	Normalization
+		if(h_mc->GetEntries() > 0)
+			h_mc->Scale(h_da->GetEntries() / h_mc->GetEntries());
+
+		c_pile[i] = canvas_pile(var[i], h_mc, h_da);
+		c_pile[i]->Write();
 	}
+	
+	// 	Combine 4 canvases into 1
+	for(int i = 0; i < number_of_hists / 4 + 1; i++){
+		TCanvas *C = new TCanvas(Form("%s_canvas_%d", option, i + 1));
+		C->Divide(2, 2);
+		for(int j = 0; j < 4; j++){
+			C->cd(j + 1);
+			if(4 * i + j < number_of_hists)
+				c_pile[4 * i + j]->DrawClonePad();
+		}
+		C->Write();
+		C->SaveAs(Form("pdf/%s_canvas_%d.pdf", option, i + 1));
+	} 
 }
 
 
@@ -125,34 +119,32 @@ void hist_compare()
 	TDirectory *DA_XX = DA_ROOT->GetDirectory("OtherVars");
 	
 	//	Names of the histograms
-	string hist_mcn[14] = {"hN10n_mcn", "hNcS_mcn", "hN300S_mcn", "hnDwall_mcn", "hnTwall_mcn", "hlnAccep_mcn", "hllrCAng_mcn", "hBeta1_mcn", "hBeta2_mcn", "hBeta3_mcn", "hBeta4_mcn", "hBeta5_mcn", "htSD_mcn", "htSDMin6_mcn"};
-	string hist_bkg[14];
-	string hist_name[28];
+	string i_var[14] = {"hN10n", "hNcS", "hN300S", "hnDwall", "hnTwall", "hlnAccep", "hllrCAng", "hBeta1", "hBeta2", "hBeta3", "hBeta4", "hBeta5", "htSD", "htSDMin6"};
+	string o_var[17] = {"hnp", "hnn", "hn10", "hnnout", "hctime_ps", "hctime_nn", "hnpdist_ps", "hnpdist_nn", "hmcttotmcn", "hmctnpdist_ps", "hmctnpdist_nn", "hnvtxdist_ps", "hnvtxdist_nn", "hmctdt0_ps", "hmctdt0_nn", "hctimeL_ps", "hctimeL_nn"};
 
-	//	Setting the array "hist_name" 
-	for(int i = 0; i < 14; i++){
-		hist_mcn[i] = hist_mcn[i].substr(0, hist_mcn[i].length() - 4);
-		hist_bkg[i] = hist_mcn[i];
-		hist_bkg[i] += "_bkg";
-		hist_mcn[i] += "_mcn";
-		
-		hist_name[i] = hist_mcn[i];
-		hist_name[i + 14] = hist_bkg[i];
-	}
-	
+	vector<string> in_var(i_var, i_var + 14);
+	vector<string> other_var(o_var, o_var + 17);
+
 	// 	Output
 	TFile *out = new TFile("output.root", "recreate");
 	
 	TDirectory *PS_OUT = out->mkdir("InVarsPS");
 	TDirectory *NN_OUT = out->mkdir("InVarsNN");
-	
+	TDirectory *XX_OUT = out->mkdir("OtherVars");
+
 	// 	Write histograms
 	PS_OUT->cd();
-	write_histograms(hist_name, MC_PS, DA_PS);
+	write_histograms(in_var, MC_PS, DA_PS, "PS");
 	
 	NN_OUT->cd();
-	write_histograms(hist_name, MC_NN, DA_NN);
+	write_histograms(in_var, MC_NN, DA_NN, "NN");
 
+	XX_OUT->cd();
+	write_histograms(other_var, MC_XX, DA_XX, "other");
+
+	for(int i = 0; i < 14; i++){
+	
+	}
 	out->Close();
 }
 
@@ -160,6 +152,7 @@ void hist_compare()
 int main()
 {
 	hist_compare();
+	return 0;
 }
 
 
